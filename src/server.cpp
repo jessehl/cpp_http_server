35981a0602639b1bf88e08e01854834eb76fdfa1
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-06 08:23:32 +0200

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -1,15 +1,77 @@
 #include "crow.h"
+#include <string>
+#include <vector>
+#include <boost/thread.hpp>
+
+std::vector<std::string> locks;
+boost::mutex searchLock;
+
+bool getLock(std::string lockName) 
+{
+  searchLock.lock();
+  for (long unsigned int i = 0; i < locks.size(); i++) 
+  {
+    if (lockName == locks[i]) 
+    {
+      return false;
+    }
+  }
+
+  locks.push_back(lockName);
+  return true;
+}
+
+bool releaseLock(std::string lockName)
+{
+  searchLock.lock();
+  for (long unsigned int i = 0; i < locks.size(); i++) 
+  {
+    if (lockName == locks[i]) 
+    {
+      locks.erase(locks.begin() + i);
+      searchLock.unlock();
+      return true;
+    }
+  }
+  searchLock.unlock();
+  return false;
+}
 
 int main()
 {
+    
     crow::SimpleApp app;
-
-    CROW_ROUTE(app, "/")([](){
-        return "Hello, LyingDutch";
+    CROW_ROUTE(app, "/")([]()
+    {
+        return "Hello LockWorld";
     });
 
-    CROW_ROUTE(app, "/status")([](){
-        return "all good";
+    CROW_ROUTE(app, "/getLock/<string>") ( [](std::string lockName) 
+    {   
+        if (!getLock(lockName)) 
+        {
+          searchLock.unlock();
+          return "false";
+        }
+
+        else 
+        {          
+          searchLock.unlock();
+          return "true";
+        }
+    });
+
+    CROW_ROUTE(app, "/releaseLock/<string>") ([](std::string lockName)
+    { 
+        if (releaseLock(lockName)) 
+        { 
+          return "Released";
+        }
+
+        else 
+        {
+          return "404 Lock not Found";
+        }
     });
 
     // run on port 8080
