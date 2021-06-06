0b5a1bf1b93a31a8df96056373f1d069def5143f
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-06 11:25:47 +0200

    Update LMserver.h

--- src/LMserver.h
+++ src/LMserver.h
@@ -1,1 +1,28 @@
 #pragma once
+
+class LMserver
+{
+ private:
+     boost::mutex storageMutex;
+     std::vector<std::string> lockVector;
+
+ private:
+     bool getLock(std::string lockName)
+     {
+       searchLock.lock();
+       for (long unsigned int i = 0; i < locks.size(); i++) {
+         if (lockName == locks[i]) {
+           searchLock.unlock();
+           return false;
+         }
+       }
+
+       locks.push_back(lockName);
+       searchLock.unlock();
+       return true;
+     }
+
+ public:
+
+
+};
