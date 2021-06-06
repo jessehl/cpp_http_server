510dbc380cfab10e153658acb775921df2ed92c3
LyingDutch <73080273+LyingDutch@users.noreply.github.com>
2021-06-06 11:26:39 +0200

    Update server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -1,28 +1,80 @@
-#pragma once
+#include "crow.h"
+#include <string>
+#include <vector>
+#include <boost/thread.hpp>
+#include "LMserver.h"
 
-class LMserver
+std::vector<std::string> locks;
+boost::mutex searchLock;
+
+bool getLock(std::string lockName) 
 {
- private:
-     boost::mutex storageMutex;
-     std::vector<std::string> lockVector;
+  searchLock.lock();
+  for (long unsigned int i = 0; i < locks.size(); i++) 
+  {
+    if (lockName == locks[i]) 
+    {
+      searchLock.unlock();
+      return false;
+    }
+  }
 
- private:
-     bool getLock(std::string lockName)
-     {
-       searchLock.lock();
-       for (long unsigned int i = 0; i < locks.size(); i++) {
-         if (lockName == locks[i]) {
-           searchLock.unlock();
-           return false;
-         }
-       }
+  locks.push_back(lockName);
+  searchLock.unlock();
+  return true;
+}
 
-       locks.push_back(lockName);
-       searchLock.unlock();
-       return true;
-     }
+bool releaseLock(std::string lockName)
+{
+  searchLock.lock();
+  for (long unsigned int i = 0; i < locks.size(); i++) 
+  {
+    if (lockName == locks[i]) 
+    {
+      locks.erase(locks.begin() + i);
+      searchLock.unlock();
+      return true;
+    }
+  }
+  searchLock.unlock();
+  return false;
+}
 
- public:
+int main()
+{
+    
+    crow::SimpleApp app;
+    CROW_ROUTE(app, "/")([]()
+    {
+        return "Hello LockWorld";
+    });
 
+    CROW_ROUTE(app, "/getLock/<string>") ( [](std::string lockName) 
+    {   
+        if (!getLock(lockName)) 
+        {
+          return "false";
+        }
 
-};
+        else 
+        {          
+          return "true";
+        }
+    });
+
+    CROW_ROUTE(app, "/releaseLock/<string>") ([](std::string lockName)
+    { 
+        if (releaseLock(lockName)) 
+        { 
+          return "Released";
+        }
+
+        else 
+        {
+          return "NF";
+        }
+    });
+
+    // run on port 8080
+    app.port(8000).multithreaded().run();
+}
